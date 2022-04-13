97936e9e1c985fc04a0280b8e4be542c069d199a
Efren <evcasino@crimson.ua.edu>
2022-04-12 20:06:05 -0500

    asd123

--- API/Controllers/SongsConController.cs
+++ API/Controllers/SongsConController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
     public class SongsConController : ControllerBase
     {
         // GET: api/SongsCon
-        //[EnableCors("OpenPolicy")]
+        [EnableCors("OpenPolicy")]
         [HttpGet]
         public List<Song> Get()
         {
@@ -27,7 +27,7 @@ namespace API.Controllers
 
             if(isOpen){
                 MySqlConnection conn = cs.GetConn();
-                string stm = "SELECT * FROM Songs WHERE Deleted = 'n'";
+                string stm = "SELECT * FROM Songs WHERE Deleted = 'n'"; //only deleted values
                 MySqlCommand cmd = new MySqlCommand(stm, conn);
 
                 List<Song> playlist = new List<Song>();
@@ -49,7 +49,7 @@ namespace API.Controllers
         }
 
         // GET: api/SongsCon/5
-        //[EnableCors("OpenPolicy")]
+        [EnableCors("OpenPolicy")]
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
@@ -68,7 +68,7 @@ namespace API.Controllers
         }
 
         // PUT: api/SongsCon/5
-       // [EnableCors("OpenPolicy")]
+       [EnableCors("OpenPolicy")]
         [HttpPut("{id}")]
         public void Put(int id,[FromBody] Song value) //favorite
         {
@@ -90,7 +90,7 @@ namespace API.Controllers
         }
 
         // DELETE: api/SongsCon/5
-        //[EnableCors("OpenPolicy")]
+        [EnableCors("OpenPolicy")]
         [HttpDelete("{id}")]
         public void Delete(int id) //delete
         {
